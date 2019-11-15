a1174ed992a1bfc03fe7b0a725ed3faefe37a1a3
Christopher Pedersen <chris@topherpedersen.com>
2019-11-15 06:27:08 -0600

    initial commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import {
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import { NavigationNativeContainer } from '@react-navigation/native';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <View>
+        <Button title="hello?"/>
+      </View>
+    );
+  }
+}
+
+export default App;
---
+++ README.md
@@ -0,0 +1,1 @@
+"# React-Navigation-Hello-World" 
