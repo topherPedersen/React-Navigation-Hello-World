c04a52eaf3435fd16fcec1dc4cd42d1f0708e6b0
Christopher Pedersen <chris@topherpedersen.com>
2019-11-15 09:03:31 -0600

    added top bar to go with bottom bar

--- App.js
+++ App.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
 
 class HomeScreen extends React.Component {
+
+  static navigationOptions = {
+    title: "Home",
+  };
+
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
@@ -14,6 +20,11 @@ class HomeScreen extends React.Component {
 }
 
 class SettingsScreen extends React.Component {
+
+  static navigationOptions = {
+    title: 'Settings',
+  };
+
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
@@ -23,9 +34,38 @@ class SettingsScreen extends React.Component {
   }
 }
 
+// REFERENCE (TabNavigator with Top Header Bar): https://github.com/react-navigation/react-navigation/issues/741
+
+const myTabNavigator = createBottomTabNavigator({
+  Home: HomeScreen,
+  Settings: SettingsScreen,
+});
+
+const RootStack = createStackNavigator(
+  {
+    Home: {
+      screen: myTabNavigator,
+      navigationOptions: {title: "Moolabeast"}
+    }
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+/*
 const TabNavigator = createBottomTabNavigator({
   Home: HomeScreen,
   Settings: SettingsScreen,
 });
 
 export default createAppContainer(TabNavigator);
+*/
+
+export default class App extends React.Component {
+  render() {
+    return(<AppContainer/>);
+  }
+}
